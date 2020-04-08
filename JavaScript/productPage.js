358c53765563882f54c8c9bae40a3a2e668dc51e
Radoslaw Konopka <47802853+radoslawkonopka@users.noreply.github.com>
2020-04-07 21:59:50 -0400

    Update productPage.js

--- JavaScript/productPage.js
+++ JavaScript/productPage.js
@@ -220,3 +220,20 @@ function topFunction() {
 }
 
 Search();
+function myFunction() {	
+  document.getElementById("myDropdown").classList.toggle("show");	
+}	
+
+ // Close the dropdown if the user clicks outside of it	
+window.onclick = function(event) {	
+  if (!event.target.matches('.dropbtn')) {	
+    var dropdowns = document.getElementsByClassName("dropdown-content");	
+    var i;	
+    for (i = 0; i < dropdowns.length; i++) {	
+      var openDropdown = dropdowns[i];	
+      if (openDropdown.classList.contains('show')) {	
+        openDropdown.classList.remove('show');	
+      }	
+    }	
+  }	
+} 
