6325b82368fe6a44eb0262bfb05add1003edcf50
Daniel-Pobidel <dpobidel@yahoo.com>
2020-04-13 21:39:29 -0400

    Cart features
    -updated cart table style
    -added quantity to cart table
    -calculate total price of cart
    -added -/+ buttons to table (still need to add functionality)

--- JavaScript/cartPage.js
+++ JavaScript/cartPage.js
@@ -1,13 +1,17 @@
 // Global cart List
 var list;
+var count;
+var totalprice;
 
 // Sends selected book from the list to another page
 function passInfo(clickedBook)
 {
 	// Changes from DOM object to string while conservating format
-	let book = list[clickedBook.rowIndex-1].outerHTML;
+    var book = new XMLSerializer().serializeToString(list[clickedBook.rowIndex-1])
+
 	// Stores the new string in localStorage
 	localStorage.setItem("currentBook",book); 
+    
 	// Go to new book
 	window.location.href = "reviewPage.html";
 }
@@ -16,11 +20,11 @@ function passInfo(clickedBook)
 function printTable()
 {
     // Creates a table for book list display
-	let table="<tr><th></th><th>Title</th><th>Author</th><th>Price</th></tr>";
+	let table="<tr><th></th><th>Title</th><th>Author</th><th>&nbsp;Price&nbsp;</th><th>Quantity</th></tr>";
     let i;
 	// Adds all necessary info to all books that match with the search and then displays it as table
 	for (i = 0; i <list.length; i++) { 
-		table += "<tr onclick='passInfo(this)' ref='a.html'><td id='pic'>";
+		table += "<tr><td onclick='passInfo(parentNode)' ref='a.html' id='pic'>";
 		table += "<img src='/";
 		table += list[i].getElementsByTagName("image")[0].childNodes[0].nodeValue;
 		table += "'/>";
@@ -28,11 +32,19 @@ function printTable()
 		table += list[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
 		table += "</td><td>";
         table += list[i].getElementsByTagName("author")[0].childNodes[0].nodeValue;
-		table += "</td><td>";
+		table += "</td><td><button type='button' name='minusButton'>"
+        table += " - </button>";
         table += "$";
 		table += list[i].getElementsByTagName("price")[0].childNodes[0].nodeValue;
+		table += "<button class='plus-btn' type='button' name='plusButton'>"
+        table += " + </button></td><td>";
+        table += "x";
+		table += count[i];
 		table += "</td></tr>";
 	}
+    table += "<tr><td></td><td></td><td></td><td> Total Price: </td> <td> $"
+    table += calcTotal();
+    table += "</td></tr>"
 	
 	// Display the table
 	document.getElementById("cartTable").innerHTML = table;  
@@ -44,6 +56,7 @@ function createTableList()
 {	
 	// Resets list
 	list = [];
+    count = [];
     
     //get cart from local storage
     let cart = localStorage.getItem("cart"); 
@@ -60,15 +73,29 @@ function createTableList()
         for (let z = 0; z<a.length;z++)
         {
             a[z] = a[z].split('$$$');
+            count[z] = a[z][1]; //count
             
             //grab book from 2d array and push it to list
             let book1 = new DOMParser().parseFromString((a[z][0]), "text/xml");
             list.push(book1);
         }
+        
     }
 	printTable();
+}
+
+function calcTotal()
+{
+    var totalprice = 0;
+    for(let i = 0; i<list.length; i++){
+        totalprice += (list[i].getElementsByTagName("price")[0].childNodes[0].nodeValue * count[i]);
+    }
+    totalprice = totalprice.toFixed(2);
+    console.log(totalprice);
+    return totalprice;
 }
 
 
 console.log()
 createTableList();
+calcTotal();
