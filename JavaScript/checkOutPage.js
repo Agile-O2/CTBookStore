72f46904e8aa36e2c7a5e231f5fc4fb7c39eaaf1
Radek <radek100200@gmail.com>
2020-04-30 01:17:11 -0400

    Refactoring of checkout + giftcard
    I put most checkout and giftcard methods together

--- JavaScript/checkOutPage.js
+++ JavaScript/checkOutPage.js
@@ -1,17 +1,3 @@
-// This function will return to the previous website
-function goBack() {
-	window.history.back();
-}
-
-// this window displays alert box after clicking on submit button
-function displaySuccess() {
-    if (checkIfValidInputs()){
-        sessionStorage.setItem("confirmedOrder",getInputInfo()); 
-        window.location.href = "thankYouPage.html";
-        alert("Thank you for your purchase!");
-    }
-}
-
 // Shows cart items on page
 function displayCartItems(){
 
@@ -54,17 +40,6 @@ function displayCartItems(){
     
 }
 
-// This function will hide the text field on checked
-function hideInfo() {
-	var checkBox = document.getElementById("myCheck");
-	var text = document.getElementById("text");
-	if (checkBox.checked == true){
-		text.style.display = "none";
-	} else {
-		text.style.display = "block";
-	}
-}
-
 // Hides gift card textbox on checkmark
 function hideGift() {
 	var checkBox = document.getElementById("giftCheck");
@@ -85,30 +60,13 @@ function checkIfValidInputs()
     unRedAll();
     // Check if textboxes are empty
     let result = 0;
-    result += checkEmptyTextBoxes("fname");
-    result += checkEmptyTextBoxes("email");
-    result += checkEmptyTextBoxes("adr");
-    result += checkEmptyTextBoxes("city");
-    result += checkEmptyTextBoxes("state");
-    result += checkEmptyTextBoxes("zip");
+    result += emptyShippingBoxes();
+    
     let totalLeft = getNonDollar("checkoutTotalPrice");
     // total 0 means that gift card covers entire cost, so
     // no need to enter billing and cc info
     if (totalLeft != 0)
-    {
-        result += checkEmptyTextBoxes("cname");
-        result += checkEmptyTextBoxes("ccnum");
-        result += checkEmptyTextBoxes("cvv");
-        // If billing address is different
-        if (document.getElementById("myCheck").checked != true)
-        {
-            result += checkEmptyTextBoxes("billingfname");
-            result += checkEmptyTextBoxes("billingadr");
-            result += checkEmptyTextBoxes("billingcity");
-            result += checkEmptyTextBoxes("billingstate");
-            result += checkEmptyTextBoxes("billingzip");
-        }
-    }
+        result += emptyBillingBoxes();
     if (result > 0)
     {
         alert("Please enter all necessary information");
@@ -122,9 +80,7 @@ function checkIfValidInputs()
     {
         result += checkNumbers("cvv",3);
         if (document.getElementById("myCheck").checked != true)
-        {
             result += checkNumbers("billingzip",5);
-        }
     }
     if (result > 0)
     {
@@ -202,137 +158,11 @@ function checkGiftCard()
     }
 }
 
-// Makes all textbox clear before checking them
-function unRedAll()
+// Returns the amount paid
+function getAmount()
 {
-    var textboxes = document.getElementsByTagName('input');
-    for(var i in textboxes)
-    {
-        if(textboxes[i].type == "text")
-        {
-            textboxes[i].style.borderColor="";
-        }
-    }
+    return localStorage.getItem("totalPrice");
 }
-
-// Prepares info to be stored on localstorage
-function getInputInfo(){
-    let orderInfo =[getDocVal("fname"),getDocVal("email"),localStorage.getItem("totalPrice")];
-    
-    // Turn inner elements into strings separated by $$$
-    orderInfo = orderInfo.join('$$$');
-    return orderInfo;
-}
-
-function getDocVal(doc)
-{
-    return document.getElementById(doc).value;
-}
-
-// Checks if user left an empty box
-function checkEmptyTextBoxes(textbox)
-{
-    if (document.getElementById(textbox).value == "")
-    {
-        document.getElementById(textbox).focus();
-        document.getElementById(textbox).style.borderColor  = "red";
-        return 1;
-    }
-    else
-    {
-        document.getElementById(textbox).style.borderColor  = "";
-        return 0;
-    }
-}
-// Checks if email format is good
-function validateEmail(email)
-{
-    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let emailDoc = document.getElementById(email);
-    if(emailDoc.value.match(mailformat))
-    {
-        emailDoc.style.borderColor  = "";
-        return 0;
-    }
-    else
-    {
-        emailDoc.focus();
-        emailDoc.style.borderColor  = "red";
-        return 1;
-    }
-}
-
-// Checks if enough numbers are entered
-function checkNumbers(textbox,goalLength)
-{
-    if (document.getElementById(textbox).value.length == goalLength)
-    {
-        document.getElementById(textbox).style.borderColor  = "";
-        return 0;
-    }
-    else
-    {
-        document.getElementById(textbox).focus();
-        document.getElementById(textbox).style.borderColor  = "red";
-        return 1;
-    }
-}
-
-// Checks if typed character is number
-function isNumber(evt) {
-    evt = (evt) ? evt : window.event;
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57) ){
-        return false;
-    }
-    return true;
-}
-
-// Checks if typed character is letter
-function lettersOnly() 
-{
-    var charCode = event.keyCode;
-    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8 || charCode == 32 || charCode == 39)
-        return true;
-    else
-        return false;
-}
-// Loads the page on start
-function loadCheckOutPage()
-{
-    displayCartItems();
-}
-
-//Select the year and month for the credit card
-$(document).ready(function() 
-{
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var qntYears = 10;
-    var selectYear = $("#year");
-    var selectMonth = $("#month");
-    var currentYear = new Date().getFullYear();
-
-    for (var y = 0; y < qntYears; y++)
-    {
-        let date = new Date(currentYear);
-        var yearElem = document.createElement("option");
-        yearElem.value = currentYear 
-        yearElem.textContent = currentYear;
-        selectYear.append(yearElem);
-        currentYear++;
-    } 
-
-    for (var m = 0; m < 12; m++)
-    {
-        let monthNum = new Date(2018, m).getMonth()
-        let month = monthNames[monthNum];
-        var monthElem = document.createElement("option");
-        monthElem.value = monthNum; 
-        monthElem.textContent = month;
-        selectMonth.append(monthElem);
-    }
-});
-
 
 // this function will hide the text field on checked
 function hideInfo() {
@@ -355,3 +185,10 @@ function totalCost()
     let total = cartValue + shipping + tax - giftcard;
     return total.toFixed(2);
 }
+
+// Loads the page on start
+function loadCheckOutPage()
+{
+    displayCartItems();
+}
+
--- JavaScript/giftCardPurchases.js
+++ JavaScript/giftCardPurchases.js
@@ -1,239 +1,71 @@
-// This function will return to the previous website
-function goBack() {
-	window.history.back();
-}
-
-// this window displays alert box after clicking on submit button
-function displaySuccess() {
-    if (checkIfValidInputs()){
-        sessionStorage.setItem("confirmedOrder",getInputInfo()); 
-        window.location.href = "thankYouPage.html";
-        alert("Thank you for your purchase!");
-    }
-}
-
-// this function will hide the text field on checked
-function hideInfo() {
-	var checkBox = document.getElementById("myCheck");
-	var text = document.getElementById("text");
-	if (checkBox.checked == true){
-		text.style.display = "none";
-	} else {
-		text.style.display = "block";
-	}
+function checkIfDone()
+{
+    if (checkIfAmountSelected() == false)
+        return false;
+    checkIfValidInputs();
 }
 
 // Checks if user enter the right info
 function checkIfValidInputs()
 {
+    unRedAll();
+    if (checkIfAmountSelected() == false)
+        return false;
+    
     // Check if textboxes are empty
     let result = 0;
-    result += checkEmptyTextBoxes("fname");
-    result += checkEmptyTextBoxes("email");
-    result += checkEmptyTextBoxes("adr");
-    result += checkEmptyTextBoxes("city");
-    result += checkEmptyTextBoxes("state");
-    result += checkEmptyTextBoxes("zip");
-    result += checkEmptyTextBoxes("cname");
-    result += checkEmptyTextBoxes("ccnum");
-    result += checkEmptyTextBoxes("cvv");
-    // If billing address is different
-    if (document.getElementById("myCheck").checked != true)
-    {
-        result += checkEmptyTextBoxes("billingfname");
-        result += checkEmptyTextBoxes("billingadr");
-        result += checkEmptyTextBoxes("billingcity");
-        result += checkEmptyTextBoxes("billingstate");
-        result += checkEmptyTextBoxes("billingzip");
-    }
+    result += emptyShippingBoxes();
+    result += emptyBillingBoxes();
+    
     if (result > 0)
     {
         alert("Please enter all necessary information");
         return false;
     }
+    
     // Now Checks if inputs are correct
     result += validateEmail("email");
     result += checkNumbers("zip",5);
     result += checkNumbers("cvv",3);
     if (document.getElementById("myCheck").checked != true)
-    {
-        result += validateEmail("billingemail");
         result += checkNumbers("billingzip",5);
-    }
+    
     if (result > 0)
     {
         alert("Please fix your information");
         return false;
     }
+    
     return true;
 }
 
-// Prepares info to be stored on localstorage
-function getInputInfo(){
-  
-	var radioButton = document.getElementsByName('Set1');
+// Check if amount for gift card is selected
+function checkIfAmountSelected()
+{
+    var textboxes = document.getElementsByTagName('input');
+    for(var i in textboxes)
+        if(textboxes[i].type == "radio")
+            if (textboxes[i].checked == true)
+                return true;            
+    alert("Please Select Amount");
+    return false;
+        
+}
+
+// Returns the amount paid
+function getAmount()
+{
+    var radioButton = document.getElementsByName('Set1');
 	var radioValue;
 	
-	for(var i = 0; i < radioButton.length; i++){
-		if(radioButton[i].checked){
-			radioValue = radioButton[i].value;
-		}
-	}
-	
-  let orderInfo =[getDocVal("fname"),getDocVal("email"),radioValue];
-  
-    // Turn inner elements into strings separated by $$$
-    orderInfo = orderInfo.join('$$$');
-    return orderInfo;
-}
-// To get document values shorter
-function getDocVal(doc)
-{
-    return document.getElementById(doc).value;
+	for(var i = 0; i < radioButton.length; i++)
+		if(radioButton[i].checked)
+			return radioButton[i].value;
+    return 0;
 }
 
-// Checks if user left an empty box
-function checkEmptyTextBoxes(textbox)
-{
-    if (document.getElementById(textbox).value == "")
-    {
-        document.getElementById(textbox).focus();
-        document.getElementById(textbox).style.borderColor  = "red";
-        return 1;
-    }
-    else
-    {
-        document.getElementById(textbox).style.borderColor  = "";
-        return 0;
-    }
-}
-// Checks if email format is good
-function validateEmail(email)
-{
-    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let emailDoc = document.getElementById(email);
-    if(emailDoc.value.match(mailformat))
-    {
-        emailDoc.style.borderColor  = "";
-        return 0;
-    }
-    else
-    {
-        emailDoc.focus();
-        emailDoc.style.borderColor  = "red";
-        return 1;
-    }
-}
-
-// Checks if enough numbers are entered
-function checkNumbers(textbox,goalLength)
-{
-    if (document.getElementById(textbox).value.length == goalLength)
-    {
-        document.getElementById(textbox).style.borderColor  = "";
-        return 0;
-    }
-    else
-    {
-        document.getElementById(textbox).focus();
-        document.getElementById(textbox).style.borderColor  = "red";
-        return 1;
-    }
-}
-
-// Checks if typed character is number
-function isNumber(evt) {
-    evt = (evt) ? evt : window.event;
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57) ){
-        return false;
-    }
-    return true;
-}
-
-// Checks if typed character is letter
-function lettersOnly() 
-{
-    var charCode = event.keyCode;
-    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8 || charCode == 32 || charCode == 39)
-        return true;
-    else
-        return false;
-}
 // Loads the page on start
-function loadCheckOutPage()
+function loadGiftCardPurchases()
 {
     console.log();
 }
-
-//Select the year and month for the credit card
-$(document).ready(function() {
-	const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	var qntYears = 10;
-	var selectYear = $("#year");
-	var selectMonth = $("#month");
-	var currentYear = new Date().getFullYear();
-
-	for (var y = 0; y < qntYears; y++){
-		let date = new Date(currentYear);
-		var yearElem = document.createElement("option");
-		yearElem.value = currentYear 
-		yearElem.textContent = currentYear;
-		selectYear.append(yearElem);
-		currentYear++;
-	} 
-
-	for (var m = 0; m < 12; m++){
-		let monthNum = new Date(2018, m).getMonth()
-		let month = monthNames[monthNum];
-		var monthElem = document.createElement("option");
-		monthElem.value = monthNum; 
-		monthElem.textContent = month;
-		selectMonth.append(monthElem);
-	}
-
-	var d = new Date();
-	var month = d.getMonth();
-    var year = d.getFullYear();
-
-    selectYear.val(year); 
-    selectYear.on("change", AdjustDays);  
-    selectMonth.val(month);    
-    selectMonth.on("change", AdjustDays);
-
-    AdjustDays();
-    selectDay.val(day)
-
-	function AdjustDays(){
-		var year = selectYear.val();
-		var month = parseInt(selectMonth.val()) + 1;
-		selectDay.empty();
-	}    
-});
-
-//This calculates the gift card price
-function setRadios()
-{
-	function sumRadios()
-	{
-		var total = 0, i = 1, oForm = this.form;
-		while (radgrp = oForm.elements['Set' + (i++)])
-		{
-			j = radgrp.length;
-			do
-				if (radgrp[--j].checked)
-				{
-					total += Number(radgrp[j].value);
-					break;
-				}
-			while (j);
-		}
-		oForm.elements.total.value = total.toFixed(2);
-	}
-
-var i = 0, input, inputs = document.getElementById('gitField').getElementsByTagName('input');
-while (input = inputs.item(i++))
-	if (input.name.match(/^Set\d+$/))
-		input.onclick = sumRadios;
-}
-onload = setRadios; 
